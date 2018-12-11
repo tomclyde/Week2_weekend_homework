1d1627315df97a6be80fbef313c8065510d6a153
Tom C <tclyde@hotmail.com>
2018-12-11 13:07:34 +0000

    guest checkin/out complete

--- room.rb
+++ room.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Room
   attr_reader :room_name, :songs, :guests
 
@@ -5,6 +7,15 @@ class Room
     @room_name = room_name
     @songs = songs
     @guests = guests
+  end
+
+  def check_in_guest_to_room(guest)
+    @guests.push(guest)
+  end
+
+  def check_out_guest_from_room(guest)
+    #binding.pry
+    @guests.delete(guest)
   end
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,12 +3,21 @@ require('minitest/rg')
 require_relative('../guest')
 require_relative('../song')
 require_relative('../room')
+require 'pry'
 
 
 class TestRoom < Minitest::Test
 
   def setup
     @room_1 = Room.new("Pop_Room", [], [])
+    @guest_tom = Guest.new("Tom")
+    @guest_louise = Guest.new("Louise")
+    @guest_ryan = Guest.new("Ryan")
+    @song_jump = Song.new("Jump")
+    @song_hey_jude = Song.new("Hey Jude")
+    @song_we_will_rock_you = Song.new("We will rock you")
+    @room_2 = Room.new("Rock_Room", [@song_jump,@song_hey_jude, @song_we_will_rock_you],
+                       [@guest_tom, @guest_louise, @guest_ryan])
   end
 
   def test_room_name
@@ -21,6 +30,31 @@ class TestRoom < Minitest::Test
 
   def test_guests__empty
     assert_equal([], @room_1.guests)
+  end
+
+  def test_songs__populated
+    assert_equal("Jump", @room_2.songs[0].song_name)
+  end
+
+  def test_guests__populated
+    assert_equal("Ryan", @room_2.guests[2].name)
+  end
+
+  def test_check_in_guest_to_room
+    @room_1.check_in_guest_to_room(@guest_ryan)
+    assert_equal('Ryan', @room_1.guests[0].name)
+  end
+
+  def test_check_out_guest_from_room__last_guest
+    #binding.pry
+    @room_2.check_out_guest_from_room(@guest_ryan)
+    #binding.pry
+    assert_equal(false, @room_2.guests.include?(@guest_ryan))
+  end
+
+  def test_check_out_guest_from_room__middle_guest
+    @room_2.check_out_guest_from_room(@guest_louise)
+    assert_equal(false, @room_2.guests.include?(@guest_louise))
   end
 
 
